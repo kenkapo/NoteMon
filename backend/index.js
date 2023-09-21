b35bd185a186e9ca89b4913ef9e4cb7d7f9c5e02
Aditya <gadiitya907@gmail.com>
2023-09-21 16:46:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+const app = express();
+const port = 8080;
+import AuthRouter from "./routes/Auth.js";
+import NoteRouter from "./routes/Notes.js";
+import mongoose from 'mongoose';
+import cors from "cors";
+import { mongolink } from "./env.js";
+import path from "path";
+const __dirname=path.resolve();
+app.use(express.static(path.join(__dirname,"/frontend/build")));
+app.use(cors());
+
+app.use(express.json());
+ 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongolink);
+  console.log("database connected");
+}
+
+app.use("/user",AuthRouter.router);
+app.use("/note",NoteRouter.router);
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})  
