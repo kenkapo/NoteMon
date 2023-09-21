2633273622947d2419a41ba8495fb87349a6d94a
Aditya <gadiitya907@gmail.com>
2023-09-21 18:24:51 +0530

    Changes in env

---
+++ env.js
@@ -0,0 +1,3 @@
+export const EMAIL = "notemon.official@gmail.com"
+export const PASSWORD = "ssut hgpm lfko uugk"
+export const mongolink="mongodb+srv://gadiitya907:PG2Xpi472ixAvhCd@cluster0.jgu7vxu.mongodb.net/notes-app?retryWrites=true&w=majority"
